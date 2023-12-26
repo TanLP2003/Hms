5f1510cc11bfae72175053a840fd070ef0f8c852
eihudunn <152309550+eihudunn@users.noreply.github.com>
2023-12-26 17:54:45 +0700

    Update PostModel.java

--- infracstructure/src/main/java/introse/group20/hms/infracstructure/models/PostModel.java
+++ infracstructure/src/main/java/introse/group20/hms/infracstructure/models/PostModel.java
@@ -1,7 +1,6 @@
 package introse.group20.hms.infracstructure.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
